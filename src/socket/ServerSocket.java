c2619e1c35ac86a9b4c4062165dbadc2a17bd0a4
Will Lucena <will.lucen4@gmail.com>
2024-03-21 15:51:46 -0300

    make server resilient to disconnections

--- src/socket/ServerSocket.java
+++ src/socket/ServerSocket.java
@@ -5,42 +5,65 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.rmi.RemoteException;
 
 import chatbot.RemoteInterface;
 
-public class ServerSocket {
+public class ServerSocket implements Runnable {
     private static final int PORTA = 8081;
 
-    public static void main(String[] args) throws RemoteException {
-        SocketChatBot chatbot = new SocketChatBot();
-        new ServerSocket().start(chatbot);
+    @Override
+    public void run() {
+        SocketChatBot chatbot;
+        try {
+            chatbot = new SocketChatBot();
+            new ServerSocket().start(chatbot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void start(RemoteInterface chatInstance) {
+    public void start(RemoteInterface chatInstance) throws IOException {
+        java.net.ServerSocket serverSocket = null;
+        Socket clientSocket = null;
         try {
-            java.net.ServerSocket serverSocket = new java.net.ServerSocket(PORTA);
+            serverSocket = new java.net.ServerSocket(PORTA);
             System.out.println("Servidor iniciado. Aguardando conexões...");
-            String message;
-            while (true){
-                Socket clientSocket = serverSocket.accept();
+            String message = "";
+            
+            while (!message.equalsIgnoreCase("4")){
+                clientSocket = serverSocket.accept();
                 System.out.println("Conexão estabelecida com: " + clientSocket);
-                
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                
-                do {
+
+                while (!clientSocket.isClosed() || !message.equalsIgnoreCase("4")){
                     message = in.readLine();
                     System.out.println("ClientSocket--> " + message);
-                    String response = chatInstance.handleMessage(message);
-                    out.println(response);
-                } while (!message.trim().contains("Sair"));
 
-                in.close();
-                out.close();
-                clientSocket.close();
-                serverSocket.close();
-            }
+                    if (message == null){
+                        message = "";
+                        clientSocket.close();
+                        in.close();
+                        out.close();
+                        break;
+                    } else if (message.equalsIgnoreCase("4")){
+                        clientSocket.close();
+                        in.close();
+                        out.close();
+                        break;
+                    }
+                    else {
+                        String response = chatInstance.handleMessage(message);
+                        out.println(response);
+                    }
+                }
+                System.out.println("Conexão encerrada.");
+        }
+            
+            System.out.println("Servidor encerrado.");
+            System.exit(0);
+            clientSocket.close();
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
