9aa9f13a322e370593ceacc8f9b32a3f3c734729
giooogk <macedo1999.gk@gmail.com>
2024-03-20 22:27:24 -0300

    Interação server e client RPC/RMI

--- src/main/java/RemoteInterface.java
+++ src/main/java/RemoteInterface.java
@@ -7,4 +7,6 @@ public interface RemoteInterface extends Remote {
     String sayHello() throws RemoteException;
     String sayTudoBem() throws RemoteException;
     String bomDia() throws RemoteException;
+    String sayBay() throws RemoteException;
+    String sendMessage(String message) throws RemoteException;
 }
