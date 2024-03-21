c7045feb56bda3d9271ad811357a7c22f8f428e1
Will Lucena <will.lucen4@gmail.com>
2024-03-21 15:51:28 -0300

    add loop to multi interactions

--- src/socket/ClientSocket.java
+++ src/socket/ClientSocket.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientSocket {
     private static final int PORTA = 8081;
@@ -16,16 +17,19 @@ public class ClientSocket {
     public void start(){
         try {
             Socket socket = new Socket("localhost", PORTA);
+            Scanner scanner = new Scanner(System.in);
             System.out.println("Conectado ao servidor.");
 
+            String input = "";
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-            out.println("Olá, servidorSocket!");
-
-            String response = in.readLine();
-            System.out.println("ServerSocket--> " + response);
-
+            while (!input.equalsIgnoreCase("4"))
+            {
+                input = scanner.nextLine();
+                out.println(input);
+                String response = in.readLine();
+                System.out.println("ServerSocket--> " + response);
+            }
             in.close();
             out.close();
             socket.close();
